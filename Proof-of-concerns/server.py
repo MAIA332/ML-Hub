6ecf1c18b9d7709c81266531e0fc631e0a207bee
MAIA332 <lk.maia0505@outlook.com>
2024-05-28 12:01:29 -0300

    Proof of concerns: WS

---
+++ Proof-of-concerns/server.py
@@ -0,0 +1,26 @@
+import asyncio
+from websockets.server import serve
+
+def getIpAddress():
+    import socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ipaddress = s.getsockname()[0]
+    s.close()
+    return ipaddress
+
+ipaddress = getIpAddress()
+port = 8765
+
+async def echo(websocket):
+    async for message in websocket:
+        print("received from {}:{} : ".format(websocket.remote_address[0],websocket.remote_address[1]) + message)
+        await websocket.send(message)
+
+async def main():
+    print("Server is activated on ws://{}:{}".format(ipaddress,port))
+    #async with serve(echo, "localhost", 8765):
+    async with serve(echo, "0.0.0.0", port):
+        await asyncio.Future() 
+
+asyncio.run(main())
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ML-Hub
 Projeto que visa oferecer um hub de analises de ML (Machine Learning) para rápida e fácil submissão de dados e obtenção de insigths
+
+## Objetivos:
+
+- Ser escalável e otimizado
+- Poder ser utilizado em quase qualquer conjunto de dados
+- Oferer uma interface plug and play
